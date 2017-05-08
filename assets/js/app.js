f0ae7a4d68a4b74e28a881c6a94f7e82ef1bd5b0
kotlyarchuk <denisov39@yahoo.com>
2017-05-08 19:28:23 +0200

    v0.2
    - new game buttom
    - display result of game
    - added draw condition

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   renderField();
+
+  $("body").on("keypress", function(e){
+    if(e.key === "Enter") {
+      newGame();
+    }
+  });
 });
 
 var gameField = [[]];
@@ -40,11 +46,11 @@ function checkGame(){
   //row checkGame
   for(var i = 0; i < 3; i++){
     if ( ($("div[x=0][y="+i+"]").hasClass("x") && $("div[x=1][y="+i+"]").hasClass("x") &&  $("div[x=2][y="+i+"]").hasClass("x") )) {
-      winType.push("col", i);
+      winType.push("col", i, "Player1");
       endGame();
     }
     if ( $("div[x=0][y="+i+"]").hasClass("circle") && $("div[x=1][y="+i+"]").hasClass("circle") &&  $("div[x=2][y="+i+"]").hasClass("circle") ) {
-      winType.push("col", i);
+      winType.push("col", i, "Player2");
       endGame();
     }
   }
@@ -53,32 +59,37 @@ function checkGame(){
   //cols check
   for(var i = 0; i < 3; i++){
     if ( $("div[x="+i+"][y=0]").hasClass("x") && $("div[x="+i+"][y=1]").hasClass("x") &&  $("div[x="+i+"][y=2]").hasClass("x") ) {
-      winType.push("row", i);
+      winType.push("row", i, "Player1");
       endGame();
     }
     if ( $("div[x="+i+"][y=0]").hasClass("circle") && $("div[x="+i+"][y=1]").hasClass("circle") &&  $("div[x="+i+"][y=2]").hasClass("circle") ) {
-      winType.push("row", i);
+      winType.push("row", i, "Player2");
       endGame();
     }
   }
 
   //diagonal check
   if( $("div[x=0][y=0]").hasClass("x") && $("div[x=1][y=1]").hasClass("x") && $("div[x=2][y=2]").hasClass("x") ){
-    winType.push("diag", 1);
+    winType.push("diag", 1, "Player1");
     endGame();
   }
   if( $("div[x=0][y=0]").hasClass("circle") && $("div[x=1][y=1]").hasClass("circle") &&  $("div[x=2][y=2]").hasClass("circle") ){
-    winType.push("diag", 1);
+    winType.push("diag", 1, "Player2");
     endGame();
   }
 
   if( $("div[x=2][y=0]").hasClass("x") && $("div[x=1][y=1]").hasClass("x") && $("div[x=0][y=2]").hasClass("x") ){
-    winType.push("diag", 2);
+    winType.push("diag", 2, "Player1");
     endGame();
   }
   if( $("div[x=2][y=0]").hasClass("circle") && $("div[x=1][y=1]").hasClass("circle") &&  $("div[x=0][y=2]").hasClass("circle") ){
-    winType.push("diag", 2);
+    winType.push("diag", 2, "Player2");
     endGame();
+  }
+
+  //check for draw
+  if( $(".x, .circle").length === 9 ) {
+    drawGame();
   }
 }
 
@@ -89,11 +100,15 @@ function endGame(){
     for(var i = 0; i < 3; i++){
       $("div[x="+i+"][y="+winType[1]+"]").addClass("ychecked");
     }
+    $(".result").text(""+winType[2]+" wins!");
+    blinker();
   }
   if(winType[0] === "row"){
     for(var i = 0; i < 3; i++){
       $("div[x="+winType[1]+"][y="+i+"]").addClass("xchecked");
     }
+    $(".result").text(""+winType[2]+" wins!");
+    blinker();
   }
   if(winType[0] === "diag"){
     if(winType[1] === 1){
@@ -106,9 +121,31 @@ function endGame(){
       $("div[x=1][y=1]").addClass("diagchecked2");
       $("div[x=0][y=2]").addClass("diagchecked2");
     }
+    $(".result").text(""+winType[2]+" wins!");
+    blinker();
   }
 }
 
 function newGame(){
+  $(".container div").remove();
+  p1turn = true;
+  winType = [];
+  renderField();
+  $(".result").text("");
+}
 
+function drawGame(){
+  $(".result").text("DRAW!");
+  blinker();
+}
+
+function blinker(){
+  setInterval(function(){
+    $(".result").css("color", "rgb("+randomColor()+")");
+  }, 500);
+}
+
+function randomColor(){
+  var result = Math.ceil(Math.random()*256) + ", " + Math.ceil(Math.random()*256) + ", " + Math.ceil(Math.random()*256);
+  return result;
 }
